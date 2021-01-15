267df84b30437004d4f889c931efc0f60947e8a1
Nestor <elnestinatorHD@gmail.com>
2021-01-15 19:50:23 +0100

    Make Styled-components work and add global SEO.

---
+++ frontend/next-seo.config.js
@@ -0,0 +1,9 @@
+export default {
+    openGraph: {
+        type: 'website',
+        locale: 'en_EN',
+        url: 'http://nstlopez.com',
+        site_name: 'Nestor Lopez Portfolio',
+
+    },
+}
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,11 @@
 import { GlobalStyles } from "../styles/global-styles";
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <GlobalStyles />
       <Component {...pageProps} />
     </>
