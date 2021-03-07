a9905a42a8e9c5ddbfa786a3c681766d567da35d
Nestor <elnestinatorHD@gmail.com>
2021-03-07 20:54:02 +0100

    Added AWS S3 provider.

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -9,4 +9,15 @@ module.exports = ({ env }) => ({
       defaultReplyTo: 'nestor@nstlopez.com',
     },
   },
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: 'eu-west-3',
+      params: {
+        Bucket: 'strapiportfolioimages',
+      },
+    },
+  },
 });
