ff4e817fab3bc6a5c4d93334811369e273eb09b1
Nestor <elnestinatorHD@gmail.com>
2021-04-05 19:33:21 +0200

    refactor: new footer container still WIP

--- frontend/containers/footer.js
+++ frontend/containers/footer.js
@@ -1,25 +1,46 @@
 import { Footer } from "../components";
+import NewsletterContainer from "./newsletter";
 
 export default function FooterContainer() {
   return (
     <Footer>
       <Footer.Container>
-        <Footer.TextLink href="https://www.linkedin.com/in/nestorlopezlopez/">
-          Linkedin
-        </Footer.TextLink>
-        <Footer.TextLink href="https://github.com/nsttt">
-          Github
-        </Footer.TextLink>
-        <Footer.TextLink href="https://instagram.com/nstlopez">
-          Instagram
-        </Footer.TextLink>
-        <Footer.TextLink href="https://twitter.com/nstlopez">
-          Twitter
-        </Footer.TextLink>
-        <Footer.TextLink href="https://discord.com/invite/qDXCrK4">
-          Discord
-        </Footer.TextLink>
-        <Footer.Button href="/contact">Say hi 👋</Footer.Button>
+        <Footer.Group>
+          <Footer.Logo src="../android-chrome-512x512.png" />
+          <Footer.FocusWord>
+            NST <Footer.Title>Lopez</Footer.Title>
+          </Footer.FocusWord>
+        </Footer.Group>
+        <Footer.Bar />
+        <Footer.Links>
+          <Footer.Group>
+            <Footer.TextLink>LinkedIn</Footer.TextLink>
+            <Footer.TextLink>Github</Footer.TextLink>
+            <Footer.TextLink>Twitter</Footer.TextLink>
+            <Footer.TextLink>Discord</Footer.TextLink>
+          </Footer.Group>
+          <Footer.Group>
+            <Footer.TextLink>Instagram</Footer.TextLink>
+            <Footer.TextLink>Dev.to</Footer.TextLink>
+            <Footer.TextLink>Medium</Footer.TextLink>
+            <Footer.TextLink>Hackernoon</Footer.TextLink>
+          </Footer.Group>
+          <Footer.Group>
+            <Footer.TextLink>Home</Footer.TextLink>
+            <Footer.TextLink>Portfolio</Footer.TextLink>
+            <Footer.Project>Project A</Footer.Project>
+            <Footer.Project>Project B</Footer.Project>
+          </Footer.Group>
+          <Footer.Group>
+            <Footer.TextLink>About me</Footer.TextLink>
+            <Footer.Contact />
+          </Footer.Group>
+        </Footer.Links>
+        <Footer.Bar />
+        <Footer.Copyright>
+          Copyright {new Date().getFullYear()}. With ❤ from Nestor.
+        </Footer.Copyright>
+        <NewsletterContainer />
       </Footer.Container>
     </Footer>
   );
