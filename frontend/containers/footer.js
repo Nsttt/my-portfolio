b518b508900fdcd5376f3eee413776b03ecf673b
Nestor <elnestinatorHD@gmail.com>
2021-04-28 15:15:30 +0200

    feat: added links

--- frontend/containers/footer.js
+++ frontend/containers/footer.js
@@ -6,27 +6,41 @@ export default function FooterContainer() {
     <Footer>
       <Footer.Container>
         <Footer.Group>
-          <Footer.Logo src="../android-chrome-512x512.png" />
+          <Footer.Logo src="/android-chrome-512x512.png" />
           <Footer.FocusWord>
             NST <Footer.Title>Lopez</Footer.Title>
           </Footer.FocusWord>
           <Footer.Bar />
           <Footer.TextGroup>
             <Footer.Links>
-              <Footer.TextLink href="https://google.es">
+              <Footer.TextLink href="https://www.linkedin.com/in/nestorlopezlopez/">
                 LinkedIn
               </Footer.TextLink>
-              <Footer.TextLink>Github</Footer.TextLink>
-              <Footer.TextLink>Twitter</Footer.TextLink>
-              <Footer.TextLink>Discord</Footer.TextLink>
+              <Footer.TextLink href="https://www.github.com/Nsttt/">
+                Github
+              </Footer.TextLink>
+              <Footer.TextLink href="https://www.twitter.com/nstlopez/">
+                Twitter
+              </Footer.TextLink>
+              <Footer.TextLink href="https://discord.com/invite/qDXCrK4">
+                Discord
+              </Footer.TextLink>
             </Footer.Links>
             <Footer.Links>
-              <Footer.TextLink>Instagram</Footer.TextLink>
-              <Footer.TextLink>Dev.to</Footer.TextLink>
-              <Footer.TextLink>Medium</Footer.TextLink>
-              <Footer.TextLink>Hackernoon</Footer.TextLink>
+              <Footer.TextLink href="https://www.instagram.com/nstlopez">
+                Instagram
+              </Footer.TextLink>
+              <Footer.TextLink href="https://dev.to/nstlopez">
+                Dev.to
+              </Footer.TextLink>
+              <Footer.TextLink href="https://medium.com/@nstlopez">
+                Medium
+              </Footer.TextLink>
+              <Footer.TextLink href="https://hackernoon.com/u/nstlopez">
+                Hackernoon
+              </Footer.TextLink>
             </Footer.Links>
-            <Footer.Contact>Contact</Footer.Contact>
+            <Footer.Contact href="/contact">Contact</Footer.Contact>
           </Footer.TextGroup>
           <Footer.Bar />
         </Footer.Group>
