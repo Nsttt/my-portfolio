120cc6cb4eab7340aad92349ad2c99f109ce4599
Nestor <elnestinatorHD@gmail.com>
2021-06-09 14:34:56 +0200

    feat: fixed data for tests

---
+++ frontend/fixtures/getGallery-fixture.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    id: "5",
+    permalink: "cuarto-proyecto",
+    title: "Cuarto Proyecto",
+    subtitle: "And the last one to try",
+    image: {
+      url: "/uploads/una_taza_de_javascript_67b477d75a.png",
+    },
+  },
+  {
+    id: "4",
+    permalink: "tercer-proyecto",
+    title: "Tercer Proyecto",
+    subtitle: "and another one",
+    image: {
+      url:
+        "/uploads/68747470733a2f2f692e696d6775722e636f6d2f4b6d47434b346c2e706e67_a47e8acd5a.png",
+    },
+  },
+  {
+    id: "3",
+    permalink: "segundo-proyecto",
+    title: "Segundo Proyecto",
+    subtitle: "This is yert another test",
+    image: {
+      url:
+        "/uploads/68747470733a2f2f692e696d6775722e636f6d2f4b6d47434b346c2e706e67_a47e8acd5a.png",
+    },
+  },
+];
---
+++ frontend/fixtures/getHeroProject-fixture.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    permalink: "first-project",
+    title: "Project Title",
+    description: "A Description",
+    image: {
+      url: "/uploads/featured_171f7cfade.png",
+    },
+  },
+];
