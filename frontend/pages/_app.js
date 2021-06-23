dc58f13393700ade83f14782b187dd6259ff0f6a
Nestor <elnestinatorHD@gmail.com>
2021-06-23 16:35:50 +0200

    feat: context & menu component globally

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,18 @@
 import { DefaultSeo } from "next-seo";
 import { appWithTranslation } from "next-i18next";
 import { GlobalStyles } from "../styles/global-styles";
+import { BurgerWrapper } from "../context/burger";
+import { Menu } from "../components";
 import SEO from "../next-seo.config";
 
 const MyApp = ({ Component, pageProps }) => (
   <>
     <DefaultSeo {...SEO} />
     <GlobalStyles />
-    <Component {...pageProps} />
+    <BurgerWrapper>
+      <Menu />
+      <Component {...pageProps} />
+    </BurgerWrapper>
   </>
 );
 
