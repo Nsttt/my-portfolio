9acee03562037f53ae41f87c6b9dfec4d212c272
Nestor <elnestinatorHD@gmail.com>
2021-07-05 12:27:37 +0200

    feat: better footer and cleanup

--- frontend/containers/footer.js
+++ frontend/containers/footer.js
@@ -4,32 +4,28 @@ export default function FooterContainer() {
   return (
     <>
       <Footer>
-        <Footer.Content>
-          <Footer.Group>
-            <Footer.TextGroup>
-              <Footer.Links>
-                <Footer.TextLink href="https://www.linkedin.com/in/nestorlopezlopez/">
-                  LinkedIn
-                </Footer.TextLink>
-                <Footer.TextLink href="https://www.github.com/Nsttt/">
-                  Github
-                </Footer.TextLink>
-                <Footer.TextLink href="https://www.twitter.com/nstlopez/">
-                  Twitter
-                </Footer.TextLink>
-                <Footer.TextLink href="https://www.instagram.com/nstlopez">
-                  Instagram
-                </Footer.TextLink>
-                <Footer.TextLink href="https://dev.to/nstlopez">
-                  Dev.to
-                </Footer.TextLink>
-                <Footer.TextLink href="https://discord.gg/">
-                  Discord
-                </Footer.TextLink>
-              </Footer.Links>
-            </Footer.TextGroup>
-          </Footer.Group>
-        </Footer.Content>
+        <Footer.Group>
+          <Footer.Links>
+            <Footer.TextLink href="https://www.linkedin.com/in/nestorlopezlopez/">
+              LinkedIn
+            </Footer.TextLink>
+            <Footer.TextLink href="https://www.github.com/Nsttt/">
+              Github
+            </Footer.TextLink>
+            <Footer.TextLink href="https://www.twitter.com/nstlopez/">
+              Twitter
+            </Footer.TextLink>
+            <Footer.TextLink href="https://www.instagram.com/nstlopez">
+              Instagram
+            </Footer.TextLink>
+            <Footer.TextLink href="https://dev.to/nstlopez">
+              Dev.to
+            </Footer.TextLink>
+            <Footer.TextLink href="https://discord.gg/">
+              Discord
+            </Footer.TextLink>
+          </Footer.Links>
+        </Footer.Group>
       </Footer>
     </>
   );
