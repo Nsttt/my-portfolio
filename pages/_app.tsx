162ab36b3db7b006bfa7cfad9d0c5b2c0a23ec27
Nestor <elnestinatorHD@gmail.com>
2022-01-11 19:06:14 +0100

    feat: import bebas neue font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 
 import '../styles/globals.css';
+import '@fontsource/bebas-neue';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
