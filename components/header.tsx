708a5dbad3cbd4b7af0fbecbc295981365981dea
Nestor <elnestinatorHD@gmail.com>
2022-02-06 20:56:34 +0100

    Tweaks on header component

--- components/header.tsx
+++ components/header.tsx
@@ -2,15 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 
-interface HeaderProps {
-  currentPage: string;
-}
-
-const Header = ({ currentPage }: HeaderProps) => {
+const Header = () => {
   const { t } = useTranslation('common');
 
   return (
-    <header className="flex my-0 mx-auto justify-between items-center">
+    <header className="flex justify-between items-center my-0 mx-auto">
       <div className="flex items-center">
         <Link href="/" passHref>
           <div className="flex items-center cursor-pointer">
@@ -20,8 +16,8 @@ const Header = ({ currentPage }: HeaderProps) => {
               width={55}
               height={55}
             />
-            <h2 className="px-2 font-bebas">
-              <span className="text-bright-pink hover:text-white transition">
+            <h2 className="group pr-5 pl-2 font-bebas text-3xl">
+              <span className="text-bright-pink group-hover:text-white transition">
                 NST
               </span>
               Lopez
@@ -31,8 +27,8 @@ const Header = ({ currentPage }: HeaderProps) => {
         <Link href="/">
           <a className="px-1">{t('header_home')}</a>
         </Link>
-        <Link href="/portfolio">
-          <a className="px-1">{t('header_portfolio')}</a>
+        <Link href="/blog">
+          <a className="px-1">{t('header_blog')}</a>
         </Link>
         <Link href="/about">
           <a className="px-1">{t('header_about')}</a>
