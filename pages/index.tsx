487e6e8c70541ff5167087fe2c5ec544bf5a52a0
Nestor <elnestinatorHD@gmail.com>
2022-02-06 20:56:45 +0100

    Tweaks on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,37 @@
-import type { NextPage } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 
 import Header from '../components/header';
+import Card from '../components/card';
 
-const Home: NextPage = () => {
+export default function Home(): JSX.Element {
   const { t } = useTranslation(['home', 'common']);
 
   return (
     <>
-      <Header currentPage="home" />
+      <Header />
       <div className="flex">
-        <div className="flex flex-col my-20">
-          <h1 className="font-bebas">{t('title')}</h1>
-          <h2>Néstor López</h2>
-          <h3>Frontend Engineer at NEXIONA</h3>
+        <div className="grid grid-cols-2 gap-20">
+          <div className="flex flex-col my-20">
+            <h1 className="font-bebas text-9xl">Néstor López</h1>
+            <h3 className="text-xl">Frontend Engineer at NEXIONA</h3>
+            <h2 className="font-bebas text-4xl">{t('title')}</h2>
+          </div>
+          <div className="flex flex-col my-20">
+            <h2>Featured Posts</h2>
+            <div className="grid grid-cols-2 gap-5">
+              <Card />
+              <Card />
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
-};
+}
 
 export const getStaticProps = async ({ locale }: Record<string, string>) => ({
   props: {
     ...(await serverSideTranslations(locale, ['common', 'home'])),
   },
 });
-
-export default Home;
