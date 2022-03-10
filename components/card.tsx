9f1e74aa530cb6fd38c7d9ccc96d596fcb0d405b
Nestor <elnestinatorHD@gmail.com>
2022-03-10 19:15:48 +0100

    Add background to card

--- components/card.tsx
+++ components/card.tsx
@@ -9,13 +9,13 @@ interface CardProps {
 const Card = ({ type, id, title, description, date }: CardProps) => {
   return (
     <a href={type === 'post' ? `/blog/${id}` : `/portfolio/${id}`}>
-      <div className="flex flex-col p-5 mt-2 w-full h-48 rounded-xl border-4 border-bright-pink hover:border-white">
+      <div className="flex flex-col p-5 mt-2 w-full h-48 bg-gray-900 hover:bg-gray-900/60 rounded-xl border-4 border-bright-pink hover:border-white">
         <h3 className="font-bebas text-3xl hover:text-white cursor-pointer">
           {title}
         </h3>
         <p className="text-base text-white">{description}</p>
         <p className="pt-3 mt-auto text-xs text-gray-400">
-          {date} • 10 min read
+          {date}
         </p>
       </div>
     </a>
