421644f23a2e5a99617e3ebf9272447899c47ed4
Nestor <elnestinatorHD@gmail.com>
2022-03-23 19:21:22 +0100

    Format code, concise text and rounded images

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -21,20 +21,16 @@ export default function Portfolio({ projects }: PortfolioProps): JSX.Element {
     <>
       <Header />
       <div className="divide-y">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="font-bebas text-3xl tracking-wide leading-9 text-bright-pink sm:text-4xl sm:leading-10 md:text-6xl">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="font-bebas text-3xl leading-9 tracking-wide text-bright-pink sm:text-4xl sm:leading-10 md:text-6xl">
             Portfolio
           </h1>
           <p>
-            {
-              "A small gallery of recent projects chosen by me. I've done them all on my own and with help from amazing people around the globe."
-            }
+            A small gallery of recent projects.
+            <br />
+            I&apos;ve done them all on my own and with help from amazing people
+            around the globe.
           </p>
-          {/* <p>
-            If you like what you see, you can take a peek at my
-            <Link href="/achievements">&nbsp;achievements&nbsp;</Link>
-            page.
-          </p> */}
         </div>
         <ul>
           {projects.map((frontMatter) => {
@@ -47,13 +43,13 @@ export default function Portfolio({ projects }: PortfolioProps): JSX.Element {
                     alt={id}
                     width={768}
                     height={200}
-                    className="object-cover object-top cursor-pointer"
+                    className="cursor-pointer rounded-lg object-cover object-top"
                   />
                 </Link>
                 <article className="mt-2 space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
                   <div className="space-y-3 xl:col-span-4">
                     <div>
-                      <h3 className="font-bebas text-4xl font-bold tracking-wide leading-8 cursor-pointer">
+                      <h3 className="cursor-pointer font-bebas text-4xl font-bold leading-8 tracking-wide">
                         <Link passHref href={`/portfolio/${id}`}>
                           <span className="tracking-wide text-bright-pink">
                             {title}
@@ -62,7 +58,7 @@ export default function Portfolio({ projects }: PortfolioProps): JSX.Element {
                       </h3>
                       <p className="text-sm">{date}</p>
                     </div>
-                    <div className="text-base text-gray-500 dark:text-gray-400 prose">
+                    <div className="prose text-base text-gray-500 dark:text-gray-400">
                       {description}
                     </div>
                   </div>
