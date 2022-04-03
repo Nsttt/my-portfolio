526ed536f4c68240bd8d96b0f9b9a49dae07e30d
Nestor <elnestinatorHD@gmail.com>
2022-04-03 17:53:20 +0200

    Add links to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,22 +5,38 @@ const Footer = () => {
     <footer className="container my-7">
       <div className="flex flex-row justify-between ">
         <div className="flex flex-row gap-3">
-          <Link href="/">
-            <a className="hover:text-white">Github</a>
-          </Link>
-          <Link href="/">
-            <a className="hover:text-white">Twitter</a>
-          </Link>
-          <Link href="/">
-            <a className="hover:text-white">LinkedIn</a>
-          </Link>
-          <Link href="/">
-            <a className="hover:text-white">Mail</a>
-          </Link>
+          <a
+            className="hover:text-white"
+            href="https://github.com/Nsttt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+
+          <a
+            className="hover:text-white"
+            href="https://twitter.com/nstlopez"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            className="hover:text-white"
+            href="https://www.linkedin.com/in/nestorlopezlopez/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a className="hover:text-white" href="mailto:nestor@nstlopez.com">
+            Mail
+          </a>
         </div>
         <div className="flex flex-row">
           <Link href="/">
-            <a className="hover:text-white">    RSS</a>
+            <a className="hover:text-white"> RSS</a>
           </Link>
         </div>
       </div>
