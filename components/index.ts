b7c5a12d83d01094241e6030655003cafecdbc23
Nestor <elnestinatorHD@gmail.com>
2022-04-03 18:37:31 +0200

    Add export barrels

---
+++ components/index.ts
@@ -0,0 +1,3 @@
+export { default as Footer } from './footer';
+export { default as Header } from './header';
+export { default as Card } from './card';
---
+++ services/index.ts
@@ -0,0 +1,7 @@
+export { getAllPostsData, getPostData, getPostsIds } from './getPosts';
+export {
+  getAllProjectsData,
+  getDisplayProjectsData,
+  getProjectData,
+  getProjectsIds,
+} from './getProjects';
