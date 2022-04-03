25d148b97d9ae1db41772218f82b6abd6a3a2ff2
Nestor <elnestinatorHD@gmail.com>
2022-04-03 17:33:24 +0200

    Fix styles of card and use next/link

--- components/card.tsx
+++ components/card.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface CardProps {
   type: string;
   id: string;
@@ -8,17 +10,15 @@ interface CardProps {
 
 const Card = ({ type, id, title, description, date }: CardProps) => {
   return (
-    <a href={type === 'post' ? `/blog/${id}` : `/portfolio/${id}`}>
-      <div className="mt-2 flex h-48 w-full flex-col rounded-xl border-4 border-bright-pink bg-gray-900 p-5 hover:border-white hover:bg-gray-900/60">
-        <h3 className="cursor-pointer font-bebas text-3xl hover:text-white">
+    <Link passHref href={type === 'post' ? `/blog/${id}` : `/portfolio/${id}`}>
+      <div className="group mt-2 flex h-48 w-full cursor-pointer flex-col rounded-xl border-4 border-bright-pink bg-gray-900 p-5 transition ease-in-out hover:border-white hover:bg-gray-900/60">
+        <h3 className="font-bebas text-3xl text-bright-pink transition ease-in-out group-hover:text-white">
           {title}
         </h3>
         <p className="text-base text-white">{description}</p>
-        <p className="mt-auto pt-3 text-xs text-gray-400">
-          {date}
-        </p>
+        <p className="mt-auto pt-3 text-xs text-gray-400">{date}</p>
       </div>
-    </a>
+    </Link>
   );
 };
 
