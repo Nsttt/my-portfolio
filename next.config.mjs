a03df2b4733c10162f8c74dc603e3deba0f99b94
Nestor <elnestinatorHD@gmail.com>
2022-08-21 11:51:46 +0200

    Add plausible configs and provider

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+import { withPlausibleProxy } from 'next-plausible';
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default withPlausibleProxy()(nextConfig);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,14 @@ import SEO from '../next-seo.config';
 import '../styles/globals.css';
 import '@fontsource/bebas-neue';
 import '@fontsource/montserrat';
+import PlausibleProvider from 'next-plausible';
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <PlausibleProvider domain="nstlopez.com">
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
-    </>
+    </PlausibleProvider>
   );
 };
 
