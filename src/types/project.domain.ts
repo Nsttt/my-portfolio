9e85a02a44b3ca4b36539922b741ed0b02795640
Nestor <elnestinatorHD@gmail.com>
2022-12-27 20:33:31 +0100

    Add types to project frontmatter

--- src/types/project.domain.ts
+++ src/types/project.domain.ts
@@ -1,4 +1,10 @@
 export interface ProjectFrontmatter {
+  slug: string;
   title: string;
   image: string;
+  description: string;
+  date: string;
+  link: string;
+  source: string;
+  tags: string[];
 }
