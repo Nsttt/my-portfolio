270371edd60adbea4226e756ca2d3912f582b0ff
Nestor <elnestinatorHD@gmail.com>
2022-12-28 19:54:01 +0100

    Add rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,18 +1,25 @@
 import rss from "@astrojs/rss";
+import type { MarkdownInstance } from "astro";
+import dayjs from "dayjs";
+
+import type { PostFrontmatter } from "../types/post.domain";
+
+const postImportResult = import.meta.glob("../content/posts/*.md", {
+  eager: true,
+});
+
+const posts = Object.values(
+  postImportResult,
+) as MarkdownInstance<PostFrontmatter>[];
 
 export const get = () =>
   rss({
-    // `<title>` field in output xml
-    title: "Nstlopez",
-    // `<description>` field in output xml
+    title: "Néstor's Blog",
     description: "A humble Astronaut’s guide to the stars",
-    // base URL for RSS <item> links
-    // SITE will use "site" from your project's astro.config.
     site: import.meta.env.SITE,
-    // list of `<item>`s in output xml
-    // simple example: generate items for every md file in /src/pages
-    // see "Generating items" section for required frontmatter and advanced use cases
-    items: import.meta.glob("./**/*.md"),
-    // (optional) inject custom xml
-    customData: `<language>en-us</language>`,
+    items: posts.map((post) => ({
+      link: `blog/${post.frontmatter.slug}`,
+      title: post.frontmatter.title,
+      pubDate: dayjs(post.frontmatter.date).toDate(),
+    })),
   });
