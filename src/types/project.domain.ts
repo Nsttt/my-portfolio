1e9e1e89b91b0d67682e66df3cfa98925a1b889c
Nestor <elnestinatorHD@gmail.com>
2023-01-08 20:27:21 +0100

    Move to using mdx for projects and tweaks to card component

--- src/types/project.domain.ts
+++ src/types/project.domain.ts
@@ -1,10 +1,13 @@
 export interface ProjectFrontmatter {
-  slug: string;
-  title: string;
-  image: string;
-  description: string;
-  date: string;
-  link: string;
-  source: string;
-  tags: string[];
+  url: string;
+  frontmatter: {
+    title: string;
+    slug: string;
+    image: string;
+    description: string;
+    date: string;
+    link: string;
+    source: string;
+    tags: string[];
+  };
 }
