01c636e84d0a8fcffe7ca4518f13f428641a7d95
Nestor <elnestinatorHD@gmail.com>
2023-01-08 20:29:04 +0100

    Tweak RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,25 +1,9 @@
 import rss from "@astrojs/rss";
-import type { MarkdownInstance } from "astro";
-import dayjs from "dayjs";
-
-import type { PostFrontmatter } from "../types/post.domain";
-
-const postImportResult = import.meta.glob("../content/posts/*.md", {
-  eager: true,
-});
-
-const posts = Object.values(
-  postImportResult,
-) as MarkdownInstance<PostFrontmatter>[];
 
 export const get = () =>
   rss({
     title: "Néstor's Blog",
     description: "",
     site: import.meta.env.SITE,
-    items: posts.map((post) => ({
-      link: `blog/${post.frontmatter.slug}`,
-      title: post.frontmatter.title,
-      pubDate: dayjs(post.frontmatter.date).toDate(),
-    })),
+    items: import.meta.glob("./blog/**/*.{md,mdx}"),
   });
