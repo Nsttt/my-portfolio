49b41c3600f972c28a17d4ffdf8801c21ce83cfc
Nestor <elnestinatorHD@gmail.com>
2023-03-25 18:11:03 +0100

    Move to using content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,22 +2,29 @@ import { defineCollection, z } from "astro:content";
 // Define a schema for each collection you'd like to validate.
 const projectCollection = defineCollection({
   schema: z.object({
-    layout: z.string(),
     title: z.string(),
     description: z.string(),
     subtitle: z.string(),
-    slug: z.string(),
     link: z.string(),
     source: z.string(),
-    pubDate: z.date(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
+    pubDate: z.string(),
+    image: z.string(),
     tags: z.array(z.string()),
   }),
 });
-// Export a single `collections` object to register your collection(s)
+
+const postCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    pubDate: z.string(),
+    expectedReadTime: z.number(),
+    tags: z.array(z.string()),
+  }),
+});
+
 export const collections = {
   projects: projectCollection,
+  posts: postCollection,
 };
